09a4ef1a7fe093897da03bfcfd75a1a5ab7a57a4
Arlette95 <arlt95@hotmail.com>
2014-10-13 18:30:12 -0500

    login PHP

---
+++ tarea/bienvenido.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<title>Inicio</title>
+
+</head>
+
+<body>
+	<div class="jumbotron">
+		<h1>Bienvenido
+		</h1>
+		
+	</div>
+	<script src="js/jquery.min.js"></script>	<!--bootstrap lo necesita pra realizar acciones-->
+	<script src="js/bootstrap.js"></script>
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+      
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="img/pug.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+     <img src="img/cafe.jpg" alt="...">
+      <div class="carousel-caption">
+
+        ...
+      </div>
+    </div>
+	
+	 <div class="item">
+      <img src="img/bosque.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+	<!--tienen jerarquia !!! (igual que css) -->
+	<script src="js/jquery.min.js"></script>	<!--bootstrap lo necesita pra realizar acciones-->
+	<script src="js/bootstrap.js"></script>	
+	
+</body>
+</html>
