cdd0a78ab876367b9b1ae873f6b780b74566b897
Arlette95 <arlt95@hotmail.com>
2014-10-14 17:07:04 -0500

    nuevo

---
+++ registrarse.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<meta  charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+	<body>
+		<div class="jumbtron">
+			<h1>¿Aún no tienes una cuenta... qué esperas!? Registrate ahora</h1>
+		</div>
+	</body>
+</html>
